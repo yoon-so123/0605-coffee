58fb1256abe62dcd39d7defc3a68de71e1b0a0b4
yoon-so123 <dadada2659@gmail.com>
2025-06-10 16:40:03 +0900

    159

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,11 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "public/index1.html"),
-        sub1: resolve(__dirname, "public/index1-2.html"),
-        sub2: resolve(__dirname, "public/index2.html"),
-        sub3: resolve(__dirname, "public/index2-2.html"),
+        main: resolve(__dirname, "index.html"),
+        sub1: resolve(__dirname, "public/index1.html"),
+        sub2: resolve(__dirname, "public/index1-2.html"),
+        sub3: resolve(__dirname, "public/index2.html"),
+        sub4: resolve(__dirname, "public/index2-2.html"),
       },
     },
   },
